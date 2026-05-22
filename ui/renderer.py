eed854c9a80bb4060bf5b761b636736feb799785
IlyaBond <bondarev150906@gmail.com>
2026-05-22 14:40:30 +0300

    done draw_node and draw_edge methods in Class Render

--- ui/renderer.py
+++ ui/renderer.py
@@ -1,15 +1,27 @@
 from core.node import Node
 from core.edge import Edge
+import pygame
 
 class Renderer:
     def __init__(self, screen):
         self.screen = screen
 
     def draw_node(self, node: Node, color):
-        pass
+        pygame.draw.circle(
+            self.screen,
+            (255,255,255),
+            (node.x, node.y),
+            8
+        )
 
     def draw_edge(self, edge: Edge, color):
-        pass
+        pygame.draw.line(
+            self.screen,
+            (255, 255, 255),
+            (edge.start_node.x, edge.start_node.y),
+            (edge.end_node.x, edge.end_node.y),
+            4
+        )
 
     def draw_path(self, nodes, color):
         pass
